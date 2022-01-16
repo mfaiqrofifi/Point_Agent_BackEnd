a76ef8dcc870a85e558250cd6feb3465284a42d5
mfaiqrofifi <mfaiqrofifi@gmail.com>
2022-01-16 15:53:31 +0700

    middleware fix

--- app/main.go
+++ app/main.go
@@ -16,6 +16,7 @@ import (
 	_repositoriesRedem "Final_Project/drivers/databases/redem"
 	_repository "Final_Project/drivers/databases/users"
 	_userDB "Final_Project/drivers/databases/users"
+	"net/http"
 
 	_adminusecase "Final_Project/business/Admin"
 	_admincontroll "Final_Project/controllers/admin"
@@ -69,12 +70,70 @@ func main() {
 		SecretJWT:       viper.GetString(`jwt.secret`),
 		ExpiresDuration: viper.GetInt(`jwt.expired`),
 	}
+	// http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "https://www.google.com")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
 
+	// 	if r.Method == "OPTIONS" {
+	// 		w.Write([]byte("allowed"))
+	// 		return
+	// 	}
+
+	// 	w.Write([]byte("hello"))
+	// })
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://labstack.com", "https://labstack.net"},
+		AllowOrigins: []string{"http://localhost:3000"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
 	}))
+	// type CORSConfig struct {
+	// 	// Skipper defines a function to skip middleware.
+	// 	Skipper Skipper
 
+	// 	// AllowOrigin defines a list of origins that may access the resource.
+	// 	// Optional. Default value []string{"*"}.
+	// 	AllowOrigins []string `yaml:"allow_origins"`
+
+	// 	// AllowOriginFunc is a custom function to validate the origin. It takes the
+	// 	// origin as an argument and returns true if allowed or false otherwise. If
+	// 	// an error is returned, it is returned by the handler. If this option is
+	// 	// set, AllowOrigins is ignored.
+	// 	// Optional.
+	// 	AllowOriginFunc func(origin string) (bool, error) `yaml:"allow_origin_func"`
+
+	// 	// AllowMethods defines a list methods allowed when accessing the resource.
+	// 	// This is used in response to a preflight request.
+	// 	// Optional. Default value DefaultCORSConfig.AllowMethods.
+	// 	AllowMethods []string `yaml:"allow_methods"`
+
+	// 	// AllowHeaders defines a list of request headers that can be used when
+	// 	// making the actual request. This is in response to a preflight request.
+	// 	// Optional. Default value []string{}.
+	// 	AllowHeaders []string `yaml:"allow_headers"`
+
+	// 	// AllowCredentials indicates whether or not the response to the request
+	// 	// can be exposed when the credentials flag is true. When used as part of
+	// 	// a response to a preflight request, this indicates whether or not the
+	// 	// actual request can be made using credentials.
+	// 	// Optional. Default value false.
+	// 	AllowCredentials bool `yaml:"allow_credentials"`
+
+	// 	// ExposeHeaders defines a whitelist headers that clients are allowed to
+	// 	// access.
+	// 	// Optional. Default value []string{}.
+	// 	ExposeHeaders []string `yaml:"expose_headers"`
+
+	// 	// MaxAge indicates how long (in seconds) the results of a preflight request
+	// 	// can be cached.
+	// 	// Optional. Default value 0.
+	// 	MaxAge int `yaml:"max_age"`
+	//   }
+	//   DefaultCORSConfig = CORSConfig{
+	// 	Skipper:      DefaultSkipper,
+	// 	AllowOrigins: []string{"*"},
+	// 	AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	//   }
 	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 	myRepositoryRedem := _repositoriesRedem.NewMysqlRedemRepository(Conn)
 	redemUsecase := _redemUsecase.NewUserUseCase(myRepositoryRedem, timeoutContext)
