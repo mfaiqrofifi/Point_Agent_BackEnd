704e5fc2f269ded9b469dbeb195e71af6513f370
mfaiqrofifi <mfaiqrofifi@gmail.com>
2022-01-16 17:09:18 +0700

    allowheaders

--- app/main.go
+++ app/main.go
@@ -84,7 +84,7 @@ func main() {
 	// })
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowHeaders: []string{echo.HeaderAuthorization, echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
 	}))
 	// type CORSConfig struct {
